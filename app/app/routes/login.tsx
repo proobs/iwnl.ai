843ea11bcf6af187db148b592b379d9f4e80c721
Mahad Osman <mahados298@gmail.com>
2025-03-19 13:50:06 -0400

    create color test model

--- app/app/routes/login.tsx
+++ app/app/routes/login.tsx
@@ -1,25 +1,24 @@
-import { SignIn } from '@clerk/remix'
-import { Navbar } from '~/components/Navbar'
-import { Footer } from '~/components/ui/footer'
+import { SignIn } from "@clerk/remix";
+import { Navbar } from "~/components/Navbar";
+import { Footer } from "~/components/ui/footer";
 
 export default function Page() {
-    
   return (
     <div className="min-h-screen flex flex-col relative bg-gradient-to-br from-blue-300 via-blue-400 to-purple-500 overflow">
-    <header className="sticky top-0 z-50 bg-transparent">
-      <Navbar />
-    </header>
+      <header className="sticky top-0 z-50 bg-transparent">
+        <Navbar />
+      </header>
 
-    <main className="flex flex-1 items-center justify-center">
+      <main className="flex flex-1 items-center justify-center">
         <SignIn />
-    </main>
+      </main>
 
-    <footer className="mt-auto w-full bg-white/5 backdrop-blur-sm border-t border-white/10">
+      <footer className="mt-auto w-full bg-white/5 backdrop-blur-sm border-t border-white/10">
         <Footer
-      companyName="IWNL" 
-      copyright={`© ${new Date().getFullYear()} All rights reserved.`}
+          companyName="IWNL"
+          copyright={`© ${new Date().getFullYear()} All rights reserved.`}
         />
-    </footer>
-  </div> 
-  )
+      </footer>
+    </div>
+  );
 }
